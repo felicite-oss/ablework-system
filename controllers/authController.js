e829534b619055d49b4ff39929dfbb54efe56a6d
Your Name <yourgithubemail@gmail.com>
2026-03-13 20:21:01 +0800

    update

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const db = require('../db/db');
+
+exports.registerApplicant = async (req, res) => {
+
+  const {
+    name,
+    username,
+    email,
+    password,
+    disability_type,
+    skills,
+    education,
+    preferred_job
+  } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const userSQL = `
+    INSERT INTO users (name, username, email, password, role)
+    VALUES (?, ?, ?, ?, 'applicant')
+  `;
+
+  db.query(userSQL, [name, username, email, hashedPassword], (err, result) => {
+
+    if (err) return res.json({ message: "User registration failed" });
+
+    const user_id = result.insertId;
+
+    const applicantSQL = `
+      INSERT INTO applicants 
+      (user_id, disability_type, skills, education, preferred_job)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+      applicantSQL,
+      [user_id, disability_type, skills, education, preferred_job],
+      (err2) => {
+
+        if (err2) return res.json({ message: "Applicant profile failed" });
+
+        res.json({ message: "Registration successful" });
+
+      }
+    );
+
+  });
+
+};
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./db/db');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // test route
@@ -9,6 +12,22 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
+// example route
+app.get('/users', (req, res) => {
+
+    db.query("SELECT * FROM users", (err, result) => {
+
+        if(err){
+            return res.status(500).json(err);
+        }
+
+        res.json(result);
+
+    });
+
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
